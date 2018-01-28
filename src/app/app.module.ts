e6673c5c1e8a4ebb8e066dd80c7a14e7543436db
Janusz Ładecki <janusz.ladecki@3s.pl>
2018-01-28 22:47:32 +0100

    (!) Change - Setup Docker Api (test query)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
 
 import {AppComponent} from './app.component';
 import {HomepageComponent} from './homepage/homepage.component';
-import {ServerComponent} from './homepage/server/server.component';
 import {DockerComponent} from './homepage/server/docker/docker.component';
+import {ServerComponent} from './homepage/server/server.component';
 import {DockerApiService} from './services/DockerApi.service';
 import {DockerContainersService} from './services/DockerContainers.service';
 
@@ -18,7 +19,8 @@ import {DockerContainersService} from './services/DockerContainers.service';
         DockerComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        HttpClientModule
     ],
     providers: [DockerApiService, DockerContainersService],
     bootstrap: [AppComponent]
--- src/app/services/DockerApi.service.ts
+++ src/app/services/DockerApi.service.ts
@@ -1,3 +1,4 @@
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {DockerContainersService} from './DockerContainers.service';
 
@@ -6,11 +7,26 @@ export class DockerApiService {
     host: string;
     containers = [];
 
-    constructor(private dockerContainers: DockerContainersService) {
+    constructor(private httpClient: HttpClient,
+                private dockerContainers: DockerContainersService) {
+    }
+
+    http() {
+        const params = new HttpParams().set('all', 'true');
+        return this.httpClient.get('http://localhost:2375/containers/json', {params});
     }
 
     test() {
         console.log('DockerApiService is working ');
         this.dockerContainers.test();
+        this.http().subscribe(
+            data => {
+                console.log(data);
+            }, (e) => {
+                console.log('Error', e);
+            }, () => {
+                console.log('complete');
+            }
+        );
     }
 }
